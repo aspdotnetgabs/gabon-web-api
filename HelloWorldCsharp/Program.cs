0fabc37b7ae3d8f6cbe391b98df4224d6526aa16
Bernard Gabon <hewbertgabon@gmail.com>
2020-09-30 23:33:47 +0800

    init hello world

---
+++ HelloWorldCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Gabs
+ * Date: 9/30/2020
+ * Time: 11:32 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace HelloWorldCsharp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
